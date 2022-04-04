0a909217bf64d18696406c9d872ec6f6543b8657
Ayush Gadre <ayushgadre786@gmail.com>
2022-04-04 13:27:28 +0530

    Created Initial Website

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*=typing animation =*/
+var typed = new Typed(".typingname",{
+    strings:["","","Ayush Gadre"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Programmer","Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
